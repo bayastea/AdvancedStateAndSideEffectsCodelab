1ebe0215fa844eac5dfef6416b2cf1809f4e5681
bayastea <i.love.ama.frog@gmail.com>
2023-11-05 19:27:01 +0900

    5. LaunchedEffect と rememberUpdatedState スプラッシュの追加

--- app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,18 +20,39 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+
+        // これは常に、LandingScreenが再構成された最新のonTimeout関数を参照する。
+        // 副作用の進行中にonTimeOutが変更された時を考慮しているらしい
+        // LaunchedEffectとともに使われることが一般的らしい
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // LandingScreenのライフサイクルに合ったエフェクトを作成する。
+        // LandingScreenが再コンポーズするか、onTimeoutが変更された場合、 遅延が再び開始されることはありません。
+        LaunchedEffect(Unit) { // 定数をキーとして使用することで、副作用を1回のみトリガーする
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
+
+//            これは間違いらしい
+//            LaunchedEffect(onTimeout) {
+//                delay(SplashWaitTime) // Simulates loading things
+//                onTimeout()
+//            }
+
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
